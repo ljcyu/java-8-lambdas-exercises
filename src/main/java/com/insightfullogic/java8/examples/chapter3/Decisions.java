c73e32cabd21bf46ce203aea47aaba0b93a363dc
ljcyu <ljcyu@163.com>
2019-07-25 17:55:42 +0800

    自己改写一个map+filter的例子

--- src/main/java/com/insightfullogic/java8/examples/chapter3/Decisions.java
+++ src/main/java/com/insightfullogic/java8/examples/chapter3/Decisions.java
@@ -27,28 +27,35 @@ public class Decisions {
         // END origins_of_bands_meth_imp
     }
 
+    public Set<String> originsOfBandsBySelf(Album album) {
+        return album.getMusicianList().stream()
+                .filter(artist -> artist.getName().startsWith("The"))
+                .map(Artist::getNationality)
+                .collect(toSet());
+    }
+
     public Set<String> originsOfBands(Album album) {
         // BEGIN origins_of_bands
-Set<String> origins = album.getMusicians()
-                           .filter(artist -> artist.getName().startsWith("The"))
-                           .map(artist -> artist.getNationality())
-                           .collect(toSet());
+        Set<String> origins = album.getMusicians()
+                .filter(artist -> artist.getName().startsWith("The"))
+                .map(artist -> artist.getNationality())
+                .collect(toSet());
         // END origins_of_bands
         return origins;
     }
 
     public Set<String> originsOfBandsMisuse(Album album) {
         // BEGIN misuse
-List<Artist> musicians = album.getMusicians()
-                              .collect(toList());
+        List<Artist> musicians = album.getMusicians()
+                .collect(toList());
 
-List<Artist> bands = musicians.stream()
-                              .filter(artist -> artist.getName().startsWith("The"))
-                              .collect(toList());
+        List<Artist> bands = musicians.stream()
+                .filter(artist -> artist.getName().startsWith("The"))
+                .collect(toList());
 
-Set<String> origins = bands.stream()
-                           .map(artist -> artist.getNationality())
-                           .collect(toSet());
+        Set<String> origins = bands.stream()
+                .map(artist -> artist.getNationality())
+                .collect(toSet());
         // END misuse
         return origins;
     }
