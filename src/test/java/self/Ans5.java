20bbb1dd1b5285f46e549a313eba52afbe228a2f
ljcyu <ljcyu@163.com>
2019-09-10 07:49:25 +0800

    collect实现的找到最长名字

--- src/test/java/self/Ans5.java
+++ src/test/java/self/Ans5.java
@@ -3,6 +3,7 @@ package self;
 import org.junit.Test;
 
 import java.util.Comparator;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Ans5 {
@@ -10,10 +11,17 @@ public class Ans5 {
       "George Harrison", "Ringo Starr", "Pete Best", "Stuart Sutcliffe");
   @Test
   public void longestNameByReduce(){
-    Comparator byLength=Comparator.comparing(String::length);
+    Comparator<String> byLength=Comparator.comparing(String::length);
     String name=names.reduce((acc,val)->{
       return byLength.compare(acc,val)>0?acc:val;
     }).get();
     System.out.println(name);
+  }
+  @Test
+  public void longestNameByCollect(){
+    Comparator<String> byLength=Comparator.comparing(String::length);
+    String res=names.max(byLength).get();
+    //String res2=names.collect(Collectors.maxBy(byLength)).get();
+    System.out.println(res);
   }
 }
