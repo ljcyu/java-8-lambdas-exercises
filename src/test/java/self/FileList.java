a80a63ede2349dab6319e54e4b9a16b7d4b9df06
ljcyu <ljcyu@163.com>
2019-10-07 08:01:35 +0800

    目录流，**这种在win上用不成

--- src/test/java/self/FileList.java
+++ src/test/java/self/FileList.java
@@ -1,5 +1,20 @@
 package self;
 
-public class FileList {
+import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileList {
+  @Test
+  public void list()throws IOException {
+    Path dir=Paths.get("h:","download");
+    //*.{rar,zip}，看jdk文档**在win上用不成
+    try(DirectoryStream directoryStream=Files.newDirectoryStream(dir,"*.{zip,rar}")){
+      directoryStream.forEach(System.out::println);
+    }
+  }
 }
