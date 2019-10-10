cd971c3d766b3cabfdf832aa208f93924d67acfb
ljcyu <ljcyu@163.com>
2019-10-10 11:39:10 +0800

    相同的funtional interface，编译有歧义，要cast”

--- src/test/java/self/Ex1.java
+++ src/test/java/self/Ex1.java
@@ -1,4 +1,22 @@
 package self;
 
+import org.junit.Test;
+
+import java.util.function.Predicate;
+
+interface IntPred{
+    boolean test(Integer num);
+}
 public class Ex1 {
+    void check(Predicate<Integer> predicate){
+        System.out.println("check predicate");
+    }
+    void check(IntPred test){
+        System.out.println("check IntPred");
+    }
+    @Test
+    public void test(){
+        //check(x->x>5);//有歧义，编译有问题
+        check((IntPred) x->x>5);
+    }
 }
